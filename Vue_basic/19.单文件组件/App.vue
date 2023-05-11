fea5394145614140674a082cdbafaf7a98edddd9
SYQ <so_nuna@naver.com>
2023-05-12 04:55:54 +0800

    Vue基础——19.单文件组件

---
+++ Vue_basic/19.单文件组件/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <school></school>
+    <student></student>
+  </div>
+</template>
+
+<script>
+import school from "school";
+import student from "student";
+export default {
+  name: "App",
+  comments: { school, student },
+};
+</script>
+
+<style>
+</style>
---
+++ Vue_basic/19.单文件组件/main.js
@@ -0,0 +1,7 @@
+import App from 'App';
+
+export default {
+    el: '#root',
+    template: '<app></app>',
+    comments: { App }
+}
