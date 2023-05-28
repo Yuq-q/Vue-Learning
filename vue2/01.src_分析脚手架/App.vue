2231b3f7782e55b52290dd29f1f525473908d421
SYQ <so_nuna@naver.com>
2023-05-28 15:54:18 +0800

    Vue2——分析脚手架

---
+++ vue2/01.src_分析脚手架/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <school></school>
+    <student></student>
+  </div>
+</template>
+
+<script>
+import school from "./components/School";
+import student from "./components/Student";
+export default {
+  name: "App",
+  components: { school, student },
+};
+</script>
+
+<style>
+</style>
