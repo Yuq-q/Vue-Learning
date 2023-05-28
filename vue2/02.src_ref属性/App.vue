e52fdf6183d0c5332debed61255fc7d4e756645c
SYQ <so_nuna@naver.com>
2023-05-28 16:35:28 +0800

    Vue2——ref属性

---
+++ vue2/02.src_ref属性/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <h1 v-text="msg" ref="title"></h1>
+    <button @click="showDOM" ref="btn">点我输出上方的DOM元素</button>
+    <school ref="sch"></school>
+  </div>
+</template>
+
+<script>
+import school from "./components/school";
+export default {
+  name: "App",
+  components: { school },
+  data() {
+    return {
+      msg:"欢迎学习Vue"
+    }
+  },
+  methods: {
+    showDOM() {
+      console.log(this.$refs.title);//真实DOM元素
+      console.log(this.$refs.btn);//真实DOM元素
+      console.log(this.$refs.sch);//School组件的实例对象（vc）
+      console.log(document.getElementById('sch'));//ref与id的区别：使用id时输出School组件的真实DOM
+    }
+  },
+};
+</script>
+
+<style>
+</style>
+
+
+<!-- ## ref属性
+
+1. 被用来给元素或子组件注册引用信息（id的替代者）
+2. 应用在html标签上获取的是真实DOM元素，应用在组件标签上是组件实例对象（vc）
+3. 使用方式：
+    1. 打标识：```<h1 ref="xxx">.....</h1>``` 或 ```<School ref="xxx"></School>```
+    2. 获取：```this.$refs.xxx``` -->
---
+++ vue2/02.src_ref属性/components/school.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h2>学校名称：{{ name }}</h2>
+    <h2>学校地址：{{ address }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "School",
+  data() {
+    return {
+      name: "scu",
+      address: "四川",
+    };
+  },
+};
+</script>
+
+<style>
+</style>
