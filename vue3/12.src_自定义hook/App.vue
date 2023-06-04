8566b5423609adb7c7ee6e78a29449c9dc3aac70
SYQ <so_nuna@naver.com>
2023-06-04 17:07:34 +0800

    Vue3——12.自定义hook

---
+++ vue3/12.src_自定义hook/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <button @click="isShowDemo = !isShowDemo">切换隐藏/显示</button>
+  <demo v-if="isShowDemo" />
+  <hr>
+  <Test></Test>
+</template>
+
+<script>
+import Demo from "./components/demo";
+import Test from "./components/Test";
+import { ref } from "vue";
+
+export default {
+  name: 'App',
+  components: { Demo, Test },
+  setup() {
+    let isShowDemo = ref(true)
+    return { isShowDemo }
+  }
+}
+</script>
+
